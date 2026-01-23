b4289327e5d9a85839d77bf809d38b2d370f27f4
Sarah Gibson <sarah.gibson@eonnext.com>
2026-01-23 17:22:14 +0000

    Add github users to teams

--- tf-config/main.tf
+++ tf-config/main.tf
@@ -13,3 +13,10 @@ resource "github_team_repository" "team_repos" {
     repository = each.value.repo
     permission = each.value.role
 }
+
+resource "github_team_membership" "team_members" {
+    for_each = local.team_membership_map
+
+    team_id  = github_team.teams[each.value.team_name].id
+    username = each.value.username
+}
