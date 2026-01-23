f32e941cc6bcca2814a04809d69be9d606c562d9
Sarah Gibson <sarah.gibson@eonnext.com>
2026-01-23 15:08:04 +0000

    Properly map the teams from yaml so that for_each can be called on the team resource

--- tf-config/locals.tf
+++ tf-config/locals.tf
@@ -2,4 +2,5 @@ locals {
   yaml_path     = "${path.root}/.."
   yaml_filename = "teams.yaml"
   yaml_data     = yamldecode(file("${local.yaml_path}/${local.yaml_filename}"))
+  teams_map     = { for team in local.yaml_data : team.name => team }
 }
