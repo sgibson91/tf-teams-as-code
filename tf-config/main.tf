234deeefc60aa7715cc4532bae159fc09baf5c94
Sarah Gibson <sarah.gibson@eonnext.com>
2026-01-23 17:13:06 +0000

    Assign each team the roles and repos it has access to

--- tf-config/main.tf
+++ tf-config/main.tf
@@ -1,7 +1,15 @@
 resource "github_team" "team" {
     for_each = local.teams_map
 
-    name                      = each.value.name
-    description               = each.value.description
-    privacy                   = "closed"
+    name        = each.value.name
+    description = each.value.description
+    privacy     = "closed"
+}
+
+resource "github_team_repository" "team_repo" {
+    for_each = local.team_repo_map
+
+    team_id    = github_team.team[each.value.team_name].id
+    repository = each.value.repo
+    permission = each.value.role
 }
