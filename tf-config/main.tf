2daea85b31cab24ef18a7f78c8a619e09027fc33
Sarah Gibson <drsarahlgibson@gmail.com>
2026-02-09 10:40:24 +0000

    Edit main resources

--- tf-config/main.tf
+++ tf-config/main.tf
@@ -1,22 +1,20 @@
-resource "github_team" "teams" {
-  for_each = local.teams_map
-
-  name        = each.value.name
-  description = each.value.description
+resource "github_team" "team" {
+  name        = local.team_name
+  description = local.team_description
   privacy     = "closed"
 }
 
-resource "github_team_repository" "team_repos" {
-  for_each = local.team_repo_map
+# resource "github_team_repository" "team_repos" {
+#   for_each = local.team_repo_map
 
-  team_id    = github_team.teams[each.value.team_name].id
-  repository = each.value.repo
-  permission = each.value.role
-}
+#   team_id    = github_team.teams[each.value.team_name].id
+#   repository = each.value.repo
+#   permission = each.value.role
+# }
 
 resource "github_team_membership" "team_members" {
-  for_each = local.team_membership_map
+  for_each = local.yaml_data.members
 
-  team_id  = github_team.teams[each.value.team_name].id
-  username = each.value.username
+  team_id  = github_team.team.id
+  username = each.value
 }
