0ebe6cc1799156ce611db4618fda8094a8726f97
Sarah Gibson <drsarahlgibson@gmail.com>
2026-02-09 09:49:27 +0000

    Small edits to variables and locals

--- tf-config/variables.tf
+++ tf-config/variables.tf
@@ -6,3 +6,7 @@ variable "github_organization" {
   default     = "sgibson91-test-org"
   description = "The name/slug of the GitHub organisation to operate within"
 }
+
+variable "team" {
+  description = "The GitHub Team to manage"
+}
