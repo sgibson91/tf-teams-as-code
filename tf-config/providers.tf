ebab724bbb55ef93e5b21ac5245ca27760a873d4
Sarah Gibson <drsarahlgibson@gmail.com>
2026-02-10 18:23:40 +0000

    run tf fmt & tflint and apply fixes

--- tf-config/providers.tf
+++ tf-config/providers.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "gcs" {
-    bucket = "teams-as-code-tfstate"
+    bucket             = "teams-as-code-tfstate"
     kms_encryption_key = "projects/supple-tracker-380219/locations/europe-west2/keyRings/tfstate-encryption/cryptoKeys/tfstate-encryption-key"
   }
 
